2ee225f00cc9be753cd1f98b6a824aac6dcb361b
Yurii Mitrohin <lordroads@mail.ru>
2022-10-02 14:12:32 +0300

    Init Lesson 4

---
+++ Lesson-4/EmployeeService/EmployeeService.Database.Data/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeService.Database.Data;
+
+[Table("Employees")]
+public class Employee
+{
+    [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [ForeignKey(nameof(Department))]
+    public Guid DepartmentId { get; set; }
+
+    [ForeignKey(nameof(EmployeeType))]
+    public int EmployeeTypeID { get; set; }
+
+    [Column, StringLength(255)]
+    public string FirstName { get; set; }
+    
+    [Column, StringLength(255)]
+    public string Surname { get; set; }
+
+    [Column, StringLength(255)]
+    public string Patronymic { get; set; }
+
+    [Column(TypeName = "money")]
+    public decimal Salary { get; set; }
+
+    
+    public Department Department { get; set; }
+    public EmployeeType EmployeeType { get; set; }
+}
---
+++ Lesson-4/EmployeeService/EmployeeService/Services/Implementations/EmployeeTypeRepository.cs
@@ -0,0 +1,61 @@
+﻿using EmployeeService.Database.Data;
+
+namespace EmployeeService.Services.Implementations;
+
+public class EmployeeTypeRepository : IEmployeeTypeRepository
+{
+    private readonly EmployeeServiceDbContext _dbContext;
+
+    public EmployeeTypeRepository(EmployeeServiceDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public int Create(EmployeeType data)
+    {
+        _dbContext.EmployeeTypes.Add(data);
+        _dbContext.SaveChanges();
+
+        return data.Id;
+    }
+
+    public bool Delete(int id)
+    {
+        EmployeeType employeeType = GetById(id);
+
+        if (employeeType != null)
+        {
+            _dbContext.Remove(employeeType);
+            _dbContext.SaveChanges();
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public IList<EmployeeType> GetAll()
+    {
+        return _dbContext.EmployeeTypes.ToList();
+    }
+
+    public EmployeeType GetById(int id)
+    {
+        return _dbContext.EmployeeTypes.FirstOrDefault(x => x.Id == id);
+    }
+
+    public bool Update(EmployeeType data)
+    {
+        EmployeeType employeeType = GetById(data.Id);
+
+        if (employeeType != null)
+        {
+            employeeType.Description = data.Description;
+            var result = _dbContext.SaveChanges();
+
+            return result > 0 ? true : false;
+        }
+
+        return false;
+    }
+}
