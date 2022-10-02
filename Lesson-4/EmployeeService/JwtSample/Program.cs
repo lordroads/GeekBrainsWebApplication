c09fda3fdc8a8657fc6a38bb897dc3999f4b0d11
Yurii Mitrohin <lordroads@mail.ru>
2022-10-02 16:01:45 +0300

    Решение домашнего задания. Урок 4

---
+++ Lesson-4/EmployeeService/JwtSample/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using EmployeeService.Utils;
+using JwtSample;
+
+bool isLoop = true;
+UserServices userServices = new UserServices();
+
+while (isLoop)
+{
+    Console.WriteLine("1: Jwt Test.");
+    Console.WriteLine("2: Reg Account.");
+    int userEnter = 0;
+    int.TryParse(Console.ReadLine(), out userEnter);
+
+    switch (userEnter)
+    {
+        case 1:
+            Console.WriteLine("User name - ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("User password - ");
+            string userPassword = Console.ReadLine();
+            Console.WriteLine(userServices.Login(userName, userPassword));
+            break;
+        case 2:
+            Console.WriteLine(PasswordUtils.CreatePasswordHash(Console.ReadLine()));
+            break;
+        case 0:
+            isLoop = false;
+            break;
+        default:
+            break;
+    }
+
+    Console.WriteLine("Press any key...");
+    Console.ReadLine();
+}
+
